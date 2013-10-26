7ac82cd0e2f5ee2b65fd51f6d05ed831398988a4
Benjamin Coe <ben@yesware.com>
2013-10-25 18:00:43 -0700

    implemented valid? check.

--- spec/strategy_spec.rb
+++ spec/strategy_spec.rb
@@ -3,10 +3,44 @@
 require 'spec_helper'
 
 describe Devise::Strategies::GoogleAppsOpenSocial do
+
   describe "valid?" do
-    it "should be valid" do
-      strategy = Devise::Strategies::GoogleAppsOpenSocial.new({}, {})
-      true.should be true
+    
+    it "should return true if xoauth_public_key is set, and equal to Google's certificate name" do
+      Devise::Strategies::GoogleAppsOpenSocial.any_instance
+        .stub(:params)
+        .and_return( {
+          # Google's opensocial certificate name.
+          :xoauth_public_key => 'pub.1210278512.2713152949996518384.cer'
+        } )
+
+      strategy = Devise::Strategies::GoogleAppsOpenSocial.new({})
+
+      strategy.valid?.should be true
     end
+
+    it "should return false if xoauth_public_key is not set" do
+      Devise::Strategies::GoogleAppsOpenSocial.any_instance
+        .stub(:params)
+        .and_return( {} )
+
+      strategy = Devise::Strategies::GoogleAppsOpenSocial.new({})
+
+      strategy.valid?.should be false
+    end
+
+    it "should return false if xoauth_public_key is not Google's" do
+      Devise::Strategies::GoogleAppsOpenSocial.any_instance
+        .stub(:params)
+        .and_return( {
+          # Google's opensocial certificate name.
+          :xoauth_public_key => 'pub.9999999.2713152949996518384.cer'
+        } )
+
+      strategy = Devise::Strategies::GoogleAppsOpenSocial.new({})
+
+      strategy.valid?.should be false
+    end
+
   end
 end
