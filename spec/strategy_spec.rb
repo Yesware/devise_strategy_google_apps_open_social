bebd9cf16bf64b1b509f678812a1848a18d92c84
Benjamin Coe <ben@yesware.com>
2013-10-25 17:32:03 -0700

    wrote dummy spec.

--- spec/strategy_spec.rb
+++ spec/strategy_spec.rb
@@ -5,7 +5,8 @@ require 'spec_helper'
 describe Devise::Strategies::GoogleAppsOpenSocial do
   describe "valid?" do
     it "should be valid" do
-      'vallid'.should == 'banana'
+      strategy = Devise::Strategies::GoogleAppsOpenSocial.new({}, {})
+      true.should be true
     end
   end
 end
